8fbcfd91af35d1f3ea18872e2e70b3bbb3120894
dhilazo <davidh97a@gmail.com>
2020-11-05 17:08:51 +0100

    Make code for saving and loading trained models #14

--- face_interpolator/main.py
+++ face_interpolator/main.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
@@ -11,6 +12,7 @@ from models.cnn_vae import ConvVAE
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser = Trainer.add_argparse_args(parser)
+    parser.add_argument('--job_name', type=str)
     args = parser.parse_args()
 
     # TODO: Define config file
@@ -28,9 +30,15 @@ if __name__ == '__main__':
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    logger = TensorBoardLogger('tb_logs')
+    logger = TensorBoardLogger(f'../output/{args.job_name}/tb_logs', name='')
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        dirpath=f'../output/{args.job_name}/checkpoints',
+        filename=args.job_name + '-{epoch:02d}-{val_loss:.2f}',
+        save_top_k=3,
+        mode='min')
 
     bottleneck_size = 40
     model = ConvVAE(bottleneck_size)
-    trainer = Trainer.from_argparse_args(args, logger=logger)
+    trainer = Trainer.from_argparse_args(args, logger=logger, callbacks=[checkpoint_callback])
     trainer.fit(model, train_loader)
